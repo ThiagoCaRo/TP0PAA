c1e359a3eab35fba9a9fa71d1683eb599e079f7f
Thiago Cândido Rocha <thiagorocha192000@gmail.com>
2022-09-11 11:33:56 -0300

    Update matriz.h

--- matriz.h
+++ matriz.h
@@ -1,5 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
+typedef struct{
+    char asterisco;
+    char cruz[3][3];
+    char xis[3][3];
+}Moldes;
 
+void iniciaMoldes(Moldes *moldes);
 void iniciaMoldura(char matriz[20][80]);
+void preencheSimples(char matriz[20][80], int quantidade, Moldes moldes);
