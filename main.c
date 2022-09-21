ff46ddca80b998b5ec261957a99a4b564e9f3d10
ThiagoCaRo <thiago.c.rocha@ufv.br>
2022-09-21 17:47:48 -0300

    Alterações na main

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ void menu(){
     printf("Escolha o tipo de figura basica a ser usada para criar a obra:\n");
     printf("1 - asterisco simples.\n2 - simbolo de soma com asteriscos.\n3 - letra X com asteriscos.\n4 - figuras aleatorias\n");
     printf("5 - Monte simbolos especiais!\n");
+    printf("6 - Sair\n");
     printf("=================================\n");
 }
 
@@ -17,10 +18,8 @@ int main(){
     char *quadroArte[20][40];
     srand(time(NULL));
     iniciaMoldes(&moldes);
-    iniciaMoldura(quadro);
+    
     menu();
-    printf("Digite o numero da opcao desejada: ");
-    scanf("%d", &op);
     printf("\n");
     printf("Digite a quantidade de figuras a serem geradas (menor ou igual a 0 para aleatorio): ");
     //maximo 1404 para asterisco simples
@@ -28,47 +27,56 @@ int main(){
     if(quantidade <= 0){
         quantidade = (rand() % 100) + 1;
     }
-    switch(op){
-        case 1:
-            preencheSimples(quadro, quantidade, moldes);
-            break;
-        case 2:
-            preencheCruz(quadro, quantidade, moldes);
-            break;
-        case 3:
-            preencheXis(quadro, quantidade, moldes);
-            break;
-        case 4:
-            preencheAleatorio(quadro, quantidade, moldes);
-            break;
-        case 5:
-            iniciaMolduraArte(quadroArte);
-            preencheArte(quadroArte);
-            break;
-    }
+    while(true){
+        printf("Digite o numero da opcao desejada: ");
+        scanf("%d", &op);
+        iniciaMoldura(quadro);
+        switch(op){
+            case 1:
+                preencheSimples(quadro, quantidade, moldes);
+                break;
+            case 2:
+                preencheCruz(quadro, quantidade, moldes);
+                break;
+            case 3:
+                preencheXis(quadro, quantidade, moldes);
+                break;
+            case 4:
+                preencheAleatorio(quadro, quantidade, moldes);
+                break;
+            case 5:
+                iniciaMolduraArte(quadroArte);
+                preencheArte(quadroArte);
+                break;
+            case 6:
+                printf("Saindo do programa!!\n");
+                exit(0);
+                break;
+        }
 
-    printf("\n\n\n\n\n");
-    if(op==5){
-        for(int i=0; i<20; i++){
-            printf("\n");
-            for(int j=0;j<40;j++){
-                printf("%s",quadroArte[i][j]);
+        printf("\n\n\n\n\n");
+        if(op==5){
+            for(int i=0; i<20; i++){
+                printf("\n");
+                for(int j=0;j<40;j++){
+                    printf("%s",quadroArte[i][j]);
 
+                }
             }
         }
-    }
 
-    else{
+        else{
 
-    
-        for(int i=0; i<20; i++){
-            printf("\n");
-            for(int j=0;j<80;j++){
-                printf("%c",quadro[i][j]);
+        
+            for(int i=0; i<20; i++){
+                printf("\n");
+                for(int j=0;j<80;j++){
+                    printf("%c",quadro[i][j]);
 
+                }
             }
         }
+        printf("\n\n\n\n\n");
     }
-    printf("\n\n\n\n\n");
     return 0;
 }
